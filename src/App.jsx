cc24765f51d8df9cf88000ca3f4c50e8963c0c9e
rasedprogrammer <rasedprogrammer@gmail.com>
2024-03-25 16:23:22 +0600

    [ADD Home section with responsive]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 };
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,17 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="www.facebook.com" className="home__social-icon" target="_blank">
+        <i className="uil uil-facebook-f"></i>
+      </a>
+      <a href="www.linkedin.com" className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a href="www.gitbut.com" className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
