b44ffa54bca3c13a6f8f43aae09cd73421ba0d99
rasedprogrammer <rasedprogrammer@gmail.com>
2024-06-15 01:46:40 +0600

    [DONE] Services Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Services from "./components/Services/Services";
 import Skills from "./components/Skills/Skills";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Services />
     </>
   );
 };
